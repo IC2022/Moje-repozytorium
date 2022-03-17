82397442db1e50137e43eec234f9a000db7bd68b
IC2022 <99969892+IC2022@users.noreply.github.com>
2022-03-17 22:36:05 +0100

    Add files via upload

---
+++ algorytm liczby pierwsze/algorytm liczby pierwsze.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+
+{
+
+    int liczba;
+    int lp = 0;
+
+    int countl = 0;
+    int nextl = 2;
+    cout << "Ile liczb pierwszych chcesz wyswietlic? ";
+    cin >> liczba;
+
+    while (lp < liczba)
+    {
+        for (int d = 2; d <= nextl - 1; d++)
+            if (nextl % d == 0) { countl++; }
+        if (countl == 0) { cout << " " << nextl; lp++; }
+        nextl++;
+        countl = 0;
+
+    }
+}
