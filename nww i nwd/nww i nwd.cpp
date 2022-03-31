62ffe6691f64fff96e77539f8fe1cad7df6182a3
IC2022 <99969892+IC2022@users.noreply.github.com>
2022-03-31 22:39:14 +0200

    Add files via upload

---
+++ nww i nwd/nww i nwd.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a, b;
+    cout << "Podaj pierwsza liczbe: ";
+    cin >> a;
+    cout << "Podaj druga liczbe: ";
+    cin >> b;
+
+    int pierwszaliczba = a, drugaliczba = b;
+    while (a!= b)
+    {
+        if (b > a)
+            b -= a;
+        else
+            a -= b;
+    }
+    cout << "NWD =" << a << endl;
+    cout << "NWW =" << (pierwszaliczba * drugaliczba) / a;
+
+    return 0;
+}
